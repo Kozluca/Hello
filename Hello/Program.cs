ec38d477d6fa2a1e3a4bfb589ed06987eb2f80c5
Kozluca <okozluca.1@gmail.com>
2024-10-03 15:09:42 +0300

    Proje dosyası ekle.

---
+++ Hello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ömer Faruk Kozluca");
+            Console.WriteLine("Yazılım alanında Kariyer Hedeflediğim için Patika Bootcampteyim");
+            Console.ReadLine();
+
+        }
+    }
+}
